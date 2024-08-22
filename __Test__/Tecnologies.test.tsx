875dfdc264915133a042169a18fa45f44ba167c6
nicolas bertinat <nbertinatwork@gmail.com>
2024-08-21 22:51:33 -0300

    Arrange Deploy 5

--- __Test__/Tecnologies.test.tsx
+++ __Test__/Tecnologies.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Logos from '../src/app/components/Logos/Logos';
+import Logos from '../src/app/Components/Logos/Logos';
 
 describe('Logos Component', () => {
   test('renders the title', () => {
